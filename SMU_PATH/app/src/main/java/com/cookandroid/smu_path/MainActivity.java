7957e4e8461fd730d3a5c938b7faa557e423cb2f
eojeen01 <eojeen01@naver.com>
2022-05-18 20:50:36 +0900

    android file

---
+++ SMU_PATH/app/src/main/java/com/cookandroid/smu_path/MainActivity.java
@@ -0,0 +1,35 @@
+package com.cookandroid.smu_path;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId ()) {
+            case android.R.id.home: //툴바 뒤로가기버튼 눌렸을 때 동작
+                finish ();
+                return true;
+            default:
+                return super.onOptionsItemSelected (item);
+        }
+    }
+
+}
