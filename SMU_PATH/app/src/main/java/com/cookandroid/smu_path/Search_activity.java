ebc0343e39eba514991cea930cd5bdaec386d3c3
eojeen01 <eojeen01@naver.com>
2022-06-02 08:39:47 +0900

    restart

--- SMU_PATH/app/src/main/java/com/cookandroid/smu_path/Search_activity.java
+++ SMU_PATH/app/src/main/java/com/cookandroid/smu_path/Search_activity.java
@@ -1,16 +1,10 @@
 package com.cookandroid.smu_path;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,34 +13,21 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import java.util.List;
-
 public class Search_activity extends AppCompatActivity {
     private EditText search_icon;
-    private ImageView search_back;
-    private TextView search_info;
-    ListView lvList;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_main);
-        displayList();
-        lvList = (ListView)findViewById(R.id.lv_list);
         search_icon = (EditText) findViewById(R.id.search_icon);
-        search_back = (ImageView) findViewById(R.id.search_back);
-        search_info = (TextView) findViewById(R.id.search_info);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
         actionBar.setDisplayHomeAsUpEnabled(true);
+
         // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
-
-
-
         search_icon.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -63,22 +44,14 @@ public class Search_activity extends AppCompatActivity {
                 // search 메소드를 호출한다.
                 String text = search_icon.getText().toString();
                 search(text);
-
-
-
             }
-
-
         });
 
     }
-
     // 검색을 수행하는 메소드
     public void search(String charText) {
-            search_info.setVisibility(View.GONE);
-            search_back.setVisibility(View.GONE);
 
-    }
+        }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -90,17 +63,5 @@ public class Search_activity extends AppCompatActivity {
                 return super.onOptionsItemSelected (item);
         }
     }
-
-    void displayList(){
-        //Dbhelper의 읽기모드 객체를 가져와 SQLiteDatabase에 담아 사용준비
-        DBHelper databaseHelper = new DBHelper(getApplicationContext());
-        databaseHelper.OpenDatabaseFile();
-
-        lvList = (ListView) databaseHelper.getTableData();
-
-        databaseHelper.close();
-
-    }
-
 }
 
