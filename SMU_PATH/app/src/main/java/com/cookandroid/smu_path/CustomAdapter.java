1124c6ed4e20895624d6cbad53ce123259ba9501
eojeen01 <eojeen01@naver.com>
2022-06-04 00:04:00 +0900

    list show

---
+++ SMU_PATH/app/src/main/java/com/cookandroid/smu_path/CustomAdapter.java
@@ -0,0 +1,69 @@
+package com.cookandroid.smu_path;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+    private ArrayList<Schedule_data> arrayList;
+    private Context context;
+
+    public CustomAdapter(ArrayList<Schedule_data> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        CustomViewHolder holder = new CustomViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        holder.tv_academic_num.setText(arrayList.get(position).getAcademic_num());
+        holder.tv_department.setText(String.valueOf(arrayList.get(position).getDepartment()));
+        holder.tv_professor.setText(arrayList.get(position).getProfessor());
+        holder.tv_lecture.setText(arrayList.get(position).getLecture());
+        holder.tv_time.setText(String.valueOf(arrayList.get(position).getTime()));
+        holder.tv_lecture_room.setText(arrayList.get(position).getLecture_room());
+        holder.tv_school_year.setText(arrayList.get(position).getSchool_year());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (arrayList != null ? arrayList.size() : 0);
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_academic_num;
+        TextView tv_school_year;
+        TextView tv_lecture;
+        TextView tv_professor;
+        TextView tv_department;
+        TextView tv_lecture_room;
+        TextView tv_time;
+
+
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.tv_academic_num = itemView.findViewById(R.id.tv_academic_num);
+            this.tv_school_year = itemView.findViewById(R.id.tv_school_year);
+            this.tv_lecture = itemView.findViewById(R.id.tv_lecture);
+            this.tv_professor = itemView.findViewById(R.id.tv_professor);
+            this.tv_department = itemView.findViewById(R.id.tv_department);
+            this.tv_lecture_room = itemView.findViewById(R.id.tv_lecture_room);
+            this.tv_time = itemView.findViewById(R.id.tv_time);
+        }
+    }
+}
--- SMU_PATH/app/src/main/java/com/cookandroid/smu_path/Search_activity.java
+++ SMU_PATH/app/src/main/java/com/cookandroid/smu_path/Search_activity.java
@@ -1,10 +1,14 @@
 package com.cookandroid.smu_path;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,13 +16,70 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Search_activity extends AppCompatActivity {
     private EditText search_icon;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Schedule_data> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_main);
+
+        recyclerView = findViewById(R.id.recyclerView); // 아이디 연결
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존 성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); // User 객체를 어댑터 쪽으로 담을 어레이 리스트
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+
+        databaseReference = database.getReference("Schedule"); // DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지 않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { //반복문으로 데이터 리스트를 추출
+                    Schedule_data scheduleData = snapshot.getValue(Schedule_data.class); // User 객체에 데이터 담는다
+                    arrayList.add(scheduleData); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // db를 가져오던 중 에러 발생 시
+                Log.e("MainActivity", String.valueOf(error.toException())); // 에러문 출력
+            }
+        });
+        adapter = new CustomAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+
         search_icon = (EditText) findViewById(R.id.search_icon);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -63,5 +124,6 @@ public class Search_activity extends AppCompatActivity {
                 return super.onOptionsItemSelected (item);
         }
     }
+
 }
 
