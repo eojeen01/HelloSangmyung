3832813fcec907555444b0c1d83dd767761f8543
eojeen01 <eojeen01@naver.com>
2022-06-11 15:36:52 +0900

    android ver.6

--- SMU_PATH/app/src/main/java/com/cookandroid/smu_path/CustomAdapter.java
+++ SMU_PATH/app/src/main/java/com/cookandroid/smu_path/CustomAdapter.java
@@ -5,15 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
     private ArrayList<Schedule_data> arrayList;
     private Context context;
+    private ArrayList<String> lecList;
 
     public CustomAdapter(ArrayList<Schedule_data> arrayList, Context context) {
         this.arrayList = arrayList;
@@ -32,13 +36,52 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
 
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
-        holder.tv_academic_num.setText(arrayList.get(position).getAcademic_num());
-        holder.tv_department.setText(String.valueOf(arrayList.get(position).getDepartment()));
-        holder.tv_professor.setText(arrayList.get(position).getProfessor());
-        holder.tv_lecture.setText(arrayList.get(position).getLecture());
-        holder.tv_time.setText(String.valueOf(arrayList.get(position).getTime()));
-        holder.tv_lecture_room.setText(arrayList.get(position).getLecture_room());
-        holder.tv_school_year.setText(arrayList.get(position).getSchool_year());
+        lecList = new ArrayList<>();
+        if(arrayList.get(position).getLecture_room().contains("N")){
+            lecList.add("식물과학관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("C")){
+            lecList.add("본관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("I")){
+            lecList.add("한누리관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("E")){
+            lecList.add("송백관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("F")){
+            lecList.add("종합실기관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("D")){
+            lecList.add("디자인관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("K")){
+            lecList.add("학생회관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("B")){
+            lecList.add("상록관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("A")){
+            lecList.add("청록관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("S")){
+            lecList.add("계당관");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("K")){
+            lecList.add("스포츠센터");
+        }
+        else if(arrayList.get(position).getLecture_room().contains("Z")){
+            lecList.add("사이버강의");
+        }
+
+        holder.tv_academic_num.setText("학수번호 : "+arrayList.get(position).getAcademic_num());
+        holder.tv_department.setText("학과 : "+arrayList.get(position).getDepartment());
+        holder.tv_professor.setText("교수님 : "+arrayList.get(position).getProfessor());
+        holder.tv_lecture.setText("강의명 : "+arrayList.get(position).getLecture());
+        holder.tv_time.setText("강의시간 : "+arrayList.get(position).getTime());
+        holder.tv_lecture_room.setText("강의실 : "+arrayList.get(position).getLecture_room()+lecList);
+        holder.tv_school_year.setText("학년 : "+arrayList.get(position).getSchool_year());
+
     }
 
     @Override
